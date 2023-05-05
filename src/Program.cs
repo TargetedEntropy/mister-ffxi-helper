81bba9e28b4ed1b59526bee4b2e3557ce1f48f45
Brian Merriam <merriam@gmail.com>
2023-05-05 13:11:32 -0400

    moving functions into the class

--- src/Program.cs
+++ src/Program.cs
@@ -9,24 +9,12 @@ namespace Mister
         static void Main(string[] args)
         {
             FFXI myClass = new FFXI();
-            List<Process> processes = myClass.ReturnProcessList();
+          
 
-            Console.WriteLine("Select your Character:");
-            int count = 0;
-            foreach (Process process in processes)
-            {
-                Console.WriteLine($"{count}: {process.MainWindowTitle}");
-                count++;
-            }
-            Console.WriteLine("Name:");
-            string response = Console.ReadLine();
-            if (response == "") 
-                Environment.Exit(0);
+            // int pol_process = processes.First(a => a.MainWindowTitle == response).Id;
 
-            int pol_process = processes.First(a => a.MainWindowTitle == response).Id;
-            Console.WriteLine(pol_process);
-            EliteAPI TheInstance  = new EliteAPI(pol_process);
-            Console.WriteLine(TheInstance.Player.GetPlayerInfo().Homepoint);
+            // EliteAPI TheInstance  = new EliteAPI(pol_process);
+            // Console.WriteLine(TheInstance.Player.GetPlayerInfo().Homepoint);
 
         }
     }
--- src/mister.cs
+++ src/mister.cs
@@ -1,34 +1,60 @@
 using System;
 using System.Diagnostics;
 using System.Collections.Generic;
+using EliteMMO.API;
 
 namespace Mister
 {
     public class FFXI
     {
         public static List<Process> matchingProcesses = new List<Process>();
+        private static string CharacterName;
+
+        private static EliteAPI POL_Instance;
 
         static FFXI()
         {
             matchingProcesses = GetProcessesByName("pol");
+            CharacterName = SelectCharacter();
+            POL_Instance = CreateInstance(CharacterName);
         }
         public List<Process> ReturnProcessList()
         {
             return matchingProcesses;
         }
 
-        private static List<Process> GetProcessesByName(string processName)
-            {
-                foreach (Process process in Process.GetProcesses())
-                {
-                    if (process.ProcessName.Contains(processName))
-                    {
-                        matchingProcesses.Add(process);
-                    }
-                }
+        private static EliteAPI CreateInstance(string CharacterName)
+        {
+            int pol_process = matchingProcesses.First(a => a.MainWindowTitle == CharacterName).Id;
+            return new EliteAPI(pol_process);
+        }
 
-                return matchingProcesses;
-        }    
+        private static string SelectCharacter()
+        {
+            Console.WriteLine("Select your Character:");
+            foreach (Process process in matchingProcesses)
+            {
+                Console.WriteLine($"    {process.MainWindowTitle}");
+            }
+            Console.WriteLine("Name:");
+            string response = Console.ReadLine();
+            if (response == "")
+                Environment.Exit(0);
+            return response;
+        }
+
+        private static List<Process> GetProcessesByName(string processName)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName.Contains(processName))
+                {
+                    matchingProcesses.Add(process);
+                }
+            }
+
+            return matchingProcesses;
+        }
 
     }
 
